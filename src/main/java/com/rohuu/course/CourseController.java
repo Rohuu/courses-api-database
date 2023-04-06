a0d7d2443ce1a4926429c16adeb81dd12496ccce
Rohuu <rohuu99022@gmail.com>
2023-04-06 13:00:32 +0530

    Controller class

---
+++ src/main/java/com/rohuu/course/CourseController.java
@@ -0,0 +1,41 @@
+package com.rohuu.course;
+
+import com.rohuu.topic.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @RequestMapping("/topics/{id}/courses")
+    public List<Course> getAllCourses(@PathVariable String id){
+        return courseService.getAllCourses(id);
+    }
+
+    @RequestMapping("/topics/{topicId}/courses/{id}") // parenthesis is for variable part
+    public Optional<Course> getCourse(@PathVariable String id){ // @PathVariable tells spring that map above variable in @RequestMapping with the arguments passed which is "id"
+        return courseService.getCourse(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses")
+    public void addCourse(@RequestBody Course course, @PathVariable String topicId){
+        course.setTopic(new Topic(topicId,"",""));
+        courseService.addCourse(course);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses/{id}")
+    public void updateCourse(@RequestBody Course course, @PathVariable String topicId){
+        course.setTopic(new Topic(topicId,"",""));
+        courseService.updateCourse(course);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{topicId}/courses/{id}")
+    public void deleteCourse(@PathVariable Course course){
+        courseService.deleteCourse(course);
+    }
+}
