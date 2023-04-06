83d5547f4052b90489f74a295c51ff4ebe1930a3
Rohuu <rohuu99022@gmail.com>
2023-04-06 13:00:55 +0530

    Repository class

---
+++ src/main/java/com/rohuu/course/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.rohuu.course;
+
+import com.rohuu.topic.Topic;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends CrudRepository<Course,String> {
+        public List<Course> findByTopicId(String topicId);
+}
