bfb373aa870c75f4dbfb60445a44278cee3ef100
Rohuu <rohuu99022@gmail.com>
2023-04-06 13:00:13 +0530

    added POJO file for courses

---
+++ src/main/java/com/rohuu/course/Course.java
@@ -0,0 +1,61 @@
+package com.rohuu.course;
+import com.rohuu.topic.Topic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity    // it maps objects of this topic class to the relational database
+public class Course {
+    @Id  // declaring primary key
+    private String id;
+    private String name;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+
+    public Course(){
+
+    }
+
+    public Course(String id, String name, String description, String topicId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.topic=new Topic(topicId,"","");
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
